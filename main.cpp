015ea93ea4dd414182206ff7713d356a31222973
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2025-10-25 22:53:19 +0400

    Все сделал

--- main.cpp
+++ main.cpp
@@ -53,14 +53,19 @@ public:
   }
 
   LinkedList& operator=(const LinkedList &list) {
-    Node<T>* current = list.head;
-    head = new Node<T>(list.head->data);
-    current = current->pNext;
-    Node<T>* currentthis = head;
-    while (current != nullptr) {
-      currentthis->pNext = new Node<T>(current->data, nullptr, currentthis);
-      currentthis = currentthis->pNext;
+    if (list.head == nullptr) {
+      head = nullptr;
+    }
+    else {
+      Node<T>* current = list.head;
+      head = new Node<T>(list.head->data);
       current = current->pNext;
+      Node<T>* currentthis = head;
+      while (current != nullptr) {
+        currentthis->pNext = new Node<T>(current->data, nullptr, currentthis);
+        currentthis = currentthis->pNext;
+        current = current->pNext;
+      }
     }
     return *this;
   }
@@ -206,6 +211,16 @@ public:
     }
   }
 
+  bool is_there(const T& data) {
+    Node<T>* current = head;
+    while (current != nullptr) {
+      if (current->data == data) {
+        return true;
+      }
+      current = current->pNext;
+    }
+    return false;
+  }
 
   ~LinkedList(){
     Node<T>* current = head;
@@ -248,13 +263,44 @@ void lucky_numbers(LinkedList<T> &list, size_t size) {
 } 
 
 int main() {
-  cout << "hello workd";
-  LinkedList<int>list(25);
-  for (int i = 0; i < 25; i++) {
-    cout << list[i] << endl;
+  LinkedList<int>list1;
+  list1.push_head(3);
+  list1.push_tail(4);
+  list1.push_head(5);
+  cout << "list1=";
+  for (int i = 0; i < 3; i++) {
+    cout << list1[i] << " ";
   }
+  LinkedList<int>list2;
+  list2.push_head(14);
+  list2.push_head(22);
+  list2.push_head(list1);
+  cout <<endl<< "list2+list1=";
+  for (int i = 0; i < 5; i++) {
+    cout << list2[i] << " ";
+  }
+  list2.pop_head();
+  cout << endl << "list2(pop_head)=";
+  for (int i = 0; i < 4; i++) {
+    cout << list2[i] << " ";
+  }
+  list1.delete_node(3);
+  cout << endl << "list1(remove 3)=";
+  for (int i = 0; i < 2; i++) {
+    cout << list1[i] << " ";
+  }
+  cout <<endl<< "My task:";
+  LinkedList<int>list(25);
+  cout <<endl<< "started mas=";
+  for (int i = 0; i < 25; i++) {
+    cout << list[i] << " ";
+  }
+  LinkedList<int>started_list(25);
   lucky_numbers(list, 25);
-  for (int i = 0; i < 8; i++) {
-    cout << list[i] << endl;
+  cout << endl << "Anlucky numbers:";
+  for (int i = 0; i < 25; i++) {
+    if (!list.is_there(started_list[i])) {
+      cout << started_list[i] << " ";
+    }
   }
 }
