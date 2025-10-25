14fdbd7274c0b8ae7783f78534c5cdf65ebd2451
SlipenchukNikita <nikitaslipenchuk.70@mail.ru>
2025-10-25 13:05:15 +0400

    Добавил методы добавления в классе

--- main.cpp
+++ main.cpp
@@ -1,8 +1,82 @@
 #include <iostream>
-#include "Matrix.h"
-#include "Function.h"
 using namespace std;
 
+template <typename T>
+struct Node {
+  T data;
+  Node* pNext;
+  Node* pPrevious;
+  Node(T data = T(), Node* pNext = nullptr, Node* pPrevious = nullptr) : data(data), pNext(pNext), pPrevious(pPrevious) {};
+};
+
+template <typename T>
+class LinkedList {
+private:
+  Node<T> *head;
+public:
+  LinkedList() {
+    head = nullptr;
+  }
+
+  void push_head(T data) {
+    if (head == nullptr) {
+      head = new Node<T>(data);
+    }
+    else {
+      head->pPrevious = new Node<T>(data, head, nullptr);
+      head = head->pPrevious;
+    }
+  }
+
+  void  push_tail(T data) {
+    if (head == nullptr) {
+      head = new Node<T>(data);
+    }
+    else {
+      Node<T> *current = head;
+      while (current->pNext != nullptr) {
+        current = current->pNext;
+      }
+      current->pNext = new Node<T>(data,nullptr,current);
+    }
+  }
+
+  T operator[](size_t index) {
+    Node<T>* current = head;
+    size_t cnt = 0;
+    while (head->pNext != nullptr) {
+      if (cnt == index) {
+        return current->data;
+      }
+      cnt++;
+      current = current->pNext;
+    }
+    return NULL;
+  }
+
+  T& operator[](size_t index) {
+    Node<T>* current = head;
+    size_t cnt = 0;
+    while (head->pNext != nullptr) {
+      if (cnt == index) {
+        return current->data;
+      }
+      cnt++;
+      current = current->pNext;
+    }
+    return NULL;
+  }
+
+  ~LinkedList(){
+    delete [] head;
+  }
+};
+
 int main() {
-  example();
+  cout << "hello world";
+  LinkedList<int>list;
+  list.push_head(12);
+  list.push_tail(24);
+  list.push_head(222);
+  cout << list[1];
 }
